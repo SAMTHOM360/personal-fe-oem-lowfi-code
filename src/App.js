fd0a9a86762dc9aa9b759a225b36e98ea583b722
SAMTHOM360 <somya.ranjan@rapidsoft.co.in>
2024-05-20 18:06:18 +0530

    Code imported

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppRoutes from "./route/AppRoutes.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
